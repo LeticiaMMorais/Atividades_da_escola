3e47f32296e8f77ff08e8c81541c20466454ea41
Letícia Morais <leticialeticiamaria674@gmail.com>
2024-01-17 14:37:42 -0300

    Update ola_mundo.py
    Arrumando e adicionando algumas coisinhas

--- ola_mundo.py
+++ ola_mundo.py
@@ -1,6 +1,9 @@
 print('Hello world! :)')
 
-tudo_bem = input('\nOiiii\nTudo bem com você? (S/N)\n')
+tudo_bem = input('\nOiiii\nTudo bem com você?\n')
+print()
 
-if tudo_bem.upper() == 'S':
+if tudo_bem.upper() in 'SIM':
     print('Que bom que está bem!')
+else:
+    print('Que pena, sinto muito :<')
