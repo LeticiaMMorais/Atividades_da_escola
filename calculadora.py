3c83f3084c09082efc8a0dff9448d3f50dd67cd2
Letícia Morais <leticialeticiamaria674@gmail.com>
2024-08-12 15:10:33 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+def soma(n1,n2):
+    return n1 + n2
+
+def subtracao(n1,n2):
+    return n1 - n2
+
+def multiplicacao(n1,n2):
+    return n1 * n2
+
+def divisao(n1,n2):
+    return n1 / n2
+ 
+def resto_divisao(n1,n2):
+    return n1 % n2
+
+def exponenciacao(n1,n2):
+    return n1**n2
