e8f2ef3d1f11e02e83e21e6663f9c5a9a9fdef3a
Letícia Morais <156326197+LeticiaMMorais@users.noreply.github.com>
2024-11-19 11:57:32 -0300

    Classes criadas em aula

---
+++ Inicializar.py
@@ -0,0 +1,11 @@
+from SuperclassePessoa import Pessoa
+p1 = Pessoa('Ciro', 123, 'Rua X')
+p1.falar()
+
+from ClasseFuncionario import Funcionario
+f1 = Funcionario('Ciro', 123, 'Rua X', 'Vendedor', 400)
+f1.falar()
+
+from ClasseCliente import Cliente
+c1 = Cliente('Ciro', 123, ' Rua X', 200)
+c1.falar()
